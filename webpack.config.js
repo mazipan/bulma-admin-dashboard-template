fccf0bbe28f9797b47ff268352e382a6ee3f766a
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-09-24 19:49:10 +0700

    feat: move to pnpm

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require('path')
-const glob = require('glob')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path');
+const glob = require('glob');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const CompressionPlugin = require("compression-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin')
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 
 const SRC = path.resolve(__dirname, 'src');
 const NODE_ENV = process.env.NODE_ENV;
 
 const isDev = () => {
-  return (NODE_ENV === 'development');
+  return NODE_ENV === 'development';
 };
 
 const setPublicPath = () => {
   return isDev() ? '/' : '/bulma-admin-dashboard-template/';
 };
 
-const setPath = function(folderName) {
+const setPath = function (folderName) {
   return path.join(__dirname, folderName);
 };
 
-function extractHTML (templateFileName) {
+function extractHTML(templateFileName) {
   return new HtmlWebpackPlugin({
     filename: `${templateFileName}.html`,
     inject: true,
@@ -36,51 +36,47 @@ function extractHTML (templateFileName) {
       html5: true,
       minifyCSS: true,
       removeComments: true,
-      removeEmptyAttributes: true
+      removeEmptyAttributes: true,
     },
-    environment: process.env.NODE_ENV
+    environment: process.env.NODE_ENV,
   });
 }
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './src/main.js',
   },
   output: {
     path: isDev() ? path.resolve(__dirname) : setPath('dist'),
     publicPath: setPublicPath(),
-    filename: isDev() ? '[name].js' : '[name].[hash].js'
+    filename: isDev() ? '[name].js' : '[name].[hash].js',
   },
   mode: isDev() ? 'development' : 'production',
-  optimization:{
+  optimization: {
     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
     runtimeChunk: false,
     splitChunks: {
-      chunks: "all",
-    }
+      chunks: 'all',
+    },
   },
   resolve: {
     extensions: ['.js', '.json'],
     alias: {
-      '@': SRC
-    }
+      '@': SRC,
+    },
   },
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
-        }
+          name: '[name].[ext]?[hash]',
+        },
       },
       {
         test: /\.m?js$/,
@@ -88,25 +84,25 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: false
+    noInfo: false,
   },
   performance: {
-    hints: false
+    hints: false,
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        isStaging: (isDev() || NODE_ENV === 'staging'),
-        NODE_ENV: '"'+NODE_ENV+'"'
-      }
+        isStaging: isDev() || NODE_ENV === 'staging',
+        NODE_ENV: '"' + NODE_ENV + '"',
+      },
     }),
     extractHTML('index'),
     extractHTML('forms'),
@@ -114,16 +110,16 @@ module.exports = {
     extractHTML('tables'),
     extractHTML('ui-elements'),
     new MiniCssExtractPlugin({
-      filename: "[name].[hash].css"
+      filename: '[name].[hash].css',
     }),
-    new CopyPlugin([
-      { from: 'src/logo.png', to: '.' }
-    ]),
+    new CopyPlugin({
+      patterns: [{ from: 'src/logo.png', to: '.' }],
+		}),
     new PurgecssPlugin({
-      paths: glob.sync(`${SRC}/*`)
+      paths: glob.sync(`${SRC}/*`,  { nodir: true }),
     }),
     new CompressionPlugin({
-      algorithm: 'gzip'
-    })
-  ]
-}
+      algorithm: 'gzip',
+    }),
+  ],
+};
