b209fa56e6b3709c91e2d3a967b522e0e1758ddd
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-01-04 22:43:50 +0700

    feat: minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/img');
@@ -5,6 +7,21 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('src/js');
   eleventyConfig.addWatchTarget('src/scss');
   eleventyConfig.setTemplateFormats(['ejs']);
+
+	eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if( outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+				collapseWhitespace: true,
+				minifyJS: true
+      });
+      return minified;
+    }
+
+    return content;
+	});
 
   return {
     dir: {
