04d4a7d6a51637657deb2a856095b5edf2da9f92
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-01-04 13:02:10 +0700

    feat: add copyright

--- src/js/module-event.js
+++ src/js/module-event.js
@@ -45,10 +45,13 @@ document.addEventListener('DOMContentLoaded', function () {
   var sidebarToggler = document.querySelector('#sidebar-toggler')
   var sidebar = document.querySelector('#main-sidebar')
   var mainContent = document.querySelector('#main')
+  var footerCopyright = document.querySelector('#copyright')
 
   sidebarToggler.addEventListener('click', function () {
     sidebar.classList.toggle('closed')
     mainContent.classList.toggle('sidebar--closed')
+		footerCopyright.classList.toggle('sidebar--closed')
+
     if (sidebar.classList.contains('closed')) {
       withChildren.forEach(function (wChildrenEl) {
         wChildrenEl.classList.remove('open')
